99863aadc2637c8509f511185d59db7e4ee94750
AustinShiflet1 <ashiflet@byupathway.edu>
2025-09-20 18:20:41 -0600

    W03: Expand Inventory

--- src/js/product.js
+++ src/js/product.js
@@ -1,15 +1,11 @@
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-//Read the “product” parameter from the URL
-const productId = getParam("product");
+loadHeaderFooter();
 
-//Create ProductData instance (the source of the products)
-const dataSource = new ProductData("tents"); 
+const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-//Create ProductDetails instance
-const product = new ProductDetails(productId, dataSource);
-
-//Initialize everything
+const product = new ProductDetails(productID, dataSource);
 product.init();
